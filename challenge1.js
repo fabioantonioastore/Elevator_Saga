3b347dd16bf78d7861c7a334710344df1c967b31
Fabio Antonio Astore <132024075+FabioXXI@users.noreply.github.com>
2023-12-13 16:59:56 -0300

    Update challenge1.js

--- challenge1.js
+++ challenge1.js
@@ -1,27 +1,22 @@
 {
     init: function(elevators, floors) {
-        var getPassengers = function(elevator, maxPassengers, min, max, floors){ 
-            floors.forEach(function(floor, index){
-                for(let i = 0; i < maxPassengers; i++){
-                    floor.on("up_button_pressed down_button_pressed", function(){
-                        if(min <= floor.floorNum() && max >= floor.floorNum()){
-                            elevator.goToFloor(floor.floorNum())
-                        }
-                    })
-                }
+        let getPassengers = (elevator, floors) => {
+            floors.forEach((floor) => {
+                floor.on("up_button_pressed down_button_pressed", () => {
+                    elevator.goToFloor(floor.floorNum())
+                })
             })
         }
         
-        var empty = function(elevator){
-            let destinations = elevator.getPressedFloors()
-            destinations.forEach(function(destination, index){
+        let empty = (elevator) => {
+            elevator.getPressedFloors().forEach((destination) => {
                 elevator.goToFloor(destination)
             })
         }
         
-        elevators.forEach(function(elevator, index){
-            elevator.on("idle", function(){
-                getPassengers(elevator, elevator.maxPassengerCount(), 0, floors.length - 1, floors)
+        elevators.forEach((elevator) => {
+            getPassengers(elevator, floors)
+            elevator.on("idle", () => {
                 empty(elevator)
             })
         })
