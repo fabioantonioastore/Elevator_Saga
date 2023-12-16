4874cd47c14f6715f182eb12ea3aecba568b1801
Fabio Antonio Astore <132024075+FabioXXI@users.noreply.github.com>
2023-12-16 01:13:33 -0300

    Update challenge3.js

--- challenge3.js
+++ challenge3.js
@@ -1,11 +1,13 @@
 {
     init: function(elevators, floors) {
-        var requests = []
+        const weight = 0.6
         
-        function getPassengers(elevator, floors){
+        function getPassengers(elevator, floors, min, max){
             floors.forEach(function(floor){
                 floor.on("up_button_pressed down_button_pressed", function(){
-                    elevator.goToFloor(floor.floorNum())
+                    if(floor.floorNum() >= min && floor.floorNum() <= max && elevator.loadFactor() <= weight){
+                        elevator.goToFloor(floor.floorNum())
+                    }
                 })
             })
         }
@@ -16,12 +18,22 @@
             })
         }
         
-        elevators.forEach(function(elevator){
-            getPassengers(elevator, floors)
+        elevators.forEach(function(elevator, index){  
+            if(index == 0){
+                getPassengers(elevator, floors, 0, 3)
+            }
+            else{
+                getPassengers(elevator, floors, 4, 7)
+            }
             elevator.on("idle", function(){
                 empty(elevator)
             })
         })
+    },
+    update: function(dt, elevators, floors) {
+        
+    }
+}
     },
     update: function(dt, elevators, floors) {
         
